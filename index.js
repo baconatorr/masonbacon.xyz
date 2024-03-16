aa416ba8644593aee41f7ba8756062e59f345898
Mason Williams <154944752+baconatorr@users.noreply.github.com>
2024-03-15 21:11:21 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,14 +1,15 @@
 document.getElementById("cards").onmousemove = e => {
-    for(const card of document.getElementsByClassName("card")) {
-      const rect = card.getBoundingClientRect(),
-            x = e.clientX - rect.left,
-            y = e.clientY - rect.top;
-  
-      card.style.setProperty("--mouse-x", `${x}px`);
-      card.style.setProperty("--mouse-y", `${y}px`);
-    };
-  }
+  for(const card of document.getElementsByClassName("card")) {
+    const rect = card.getBoundingClientRect(),
+          x = e.clientX - rect.left,
+          y = e.clientY - rect.top;
 
-  function openPage(pageUrl){
-    window.open(pageUrl);
-  }
+    card.style.setProperty("--mouse-x", `${x}px`);
+    card.style.setProperty("--mouse-y", `${y}px`);
+  };
+}
+
+function pageURL(pageUrl) {
+  window.location.href = pageUrl;
+  console.log(pageUrl)
+}
